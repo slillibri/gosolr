146aab5602369b09f488360272c738fa48e06c27
slillibri <scott.lillibridge@gmail.com>
2011-08-29 23:51:37 -0700

    I forgot to gofmt on last commit. Fixed som imports for goinstall'd libs

--- gosolr.go
+++ gosolr.go
@@ -5,160 +5,161 @@
 package main
 
 import (
-    "flag"
-    "http"
-    "io"
-    "json"
-    "os"
-    "strconv"
-    "stomp"
-    "net"
-    l4g "log4go.googlecode.com/hg"
+	"flag"
+	"http"
+	"io"
+	"json"
+	"os"
+	"strconv"
+	stomp "github.com/nf/gostomp"
+	"net"
+	l4g "log4go.googlecode.com/hg"
 )
 
-var(
-    config map[string]map[string]string
-    solrServers map[string]map[string]string
+var (
+	config      map[string]map[string]string
+	solrServers map[string]map[string]string
 )
 
-type SolrPost struct{
-    Body    string
-    Authkey string
+type SolrPost struct {
+	Body    string
+	Authkey string
 }
 
 func handleRequest(w http.ResponseWriter, req *http.Request) {
-    apiKey := req.URL.Path[1:]
-    
-    if req.Method == "GET" {
-        //Need to have some cleanup and validation here
-        solrUrl := "http://" + solrServers[apiKey]["server"] + "/solr/" + solrServers[apiKey]["core"] + "/select/?" + req.URL.RawQuery
-        l4g.Debug("Proxying Request to %s for %s", solrUrl, apiKey)
-        
-        r, _, err := http.Get(solrUrl)                
-        if err != nil {
-            //Set actual error page here
-            l4g.Error("Error: %s\n", err.String())
-            http.Error(w, "500 Internal Server Error", 500)
-            return
-        }
-        l4g.Debug("Tomcat response: %s", r.Status)        
-        r.Write(w)
-        r.Body.Close()
-    }
-    if req.Method == "POST" {
-        header := req.Header
+	apiKey := req.URL.Path[1:]
 
-        //Reject non-json data - This check can probably be done at the processing layer
-        ct := header["Content-Type"][0]
-        if ct != "application/json" {
-            l4g.Error("Unsupported Content type %s", ct)
-            http.Error(w, "Unsupported content type", 400)
-            return
-        }
+	if req.Method == "GET" {
+		//Need to have some cleanup and validation here
+		solrUrl := "http://" + solrServers[apiKey]["server"] + "/solr/" + solrServers[apiKey]["core"] + "/select/?" + req.URL.RawQuery
+		l4g.Debug("Proxying Request to %s for %s", solrUrl, apiKey)
 
-        //Handle length check
-        length, err := strconv.Atoi(header["Content-Length"][0])
-        if err != nil {
-            l4g.Error("Error converting content-length header: %s", err.String())
-            http.Error(w, "Internal Server Error", 500)
-            return
-        }
-        if length > 1024*1024 {
-            l4g.Error("Post too large: %d", length)
-            http.Error(w, "Post too large", 400)
-            return
-        }
-        l4g.Debug("Post content-length: %d", length)
+		r, err := http.Get(solrUrl)
+		if err != nil {
+			//Set actual error page here
+			l4g.Error("Error: %s\n", err.String())
+			http.Error(w, "500 Internal Server Error", 500)
+			return
+		}
+		l4g.Debug("Tomcat response: %s", r.Status)
+		r.Write(w)
+		r.Body.Close()
+	}
+	if req.Method == "POST" {
+		header := req.Header
 
-        body := make([]byte, length)
-        len, ok := io.ReadFull(req.Body, body)
-        if ok != nil {
-            l4g.Debug("error reading io.ReadFull: %s", ok.String())
-            http.Error(w, "Internal Server Error", 500)
-            return
-        }
-        l4g.Debug("Content-length of %d, but read %d bytes from body", length, len)
+		//Reject non-json data - This check can probably be done at the processing layer
+		ct := header["Content-Type"][0]
+		if ct != "application/json" {
+			l4g.Error("Unsupported Content type %s", ct)
+			http.Error(w, "Unsupported content type", 400)
+			return
+		}
 
-        var message SolrPost
-        if ok := json.Unmarshal(body, &message); ok != nil {
-            l4g.Debug("Error unmarshalling json: %s", ok.String())
-            http.Error(w, "Internal Server Error", 500)
-            return
-        }
+		//Handle length check
+		length, err := strconv.Atoi(header["Content-Length"][0])
+		if err != nil {
+			l4g.Error("Error converting content-length header: %s", err.String())
+			http.Error(w, "Internal Server Error", 500)
+			return
+		}
+		if length > 1024*1024 {
+			l4g.Error("Post too large: %d", length)
+			http.Error(w, "Post too large", 400)
+			return
+		}
+		l4g.Debug("Post content-length: %d", length)
 
-        l4g.Debug("JSON message body: %s", message.Body)
+		body := make([]byte, length)
+		len, ok := io.ReadFull(req.Body, body)
+		if ok != nil {
+			l4g.Debug("error reading io.ReadFull: %s", ok.String())
+			http.Error(w, "Internal Server Error", 500)
+			return
+		}
+		l4g.Debug("Content-length of %d, but read %d bytes from body", length, len)
 
-        //Handle Auth
-        if solrServers[apiKey]["authstring"] != message.Authkey {
-            l4g.Error("Incorrect authkey")
-            http.Error(w, "Unauthorized", 401)
-            return
-        }
+		var message SolrPost
+		if ok := json.Unmarshal(body, &message); ok != nil {
+			l4g.Debug("Error unmarshalling json: %s", ok.String())
+			http.Error(w, "Internal Server Error", 500)
+			return
+		}
 
-        //Post message to queue
-        nc, err := net.Dial("tcp", "" + config["stomp"]["host"])
-        if err != nil {
-            l4g.Error("Error conneceting to queue %s", err.String())
-            http.Error(w, "Internal Server Error", 500)
-            return
-        }
+		l4g.Debug("JSON message body: %s", message.Body)
 
-        //Note, the stomp module doesn't return os.Error on Send()
-        c := stomp.Connect(nc, nil)
-        queue := "/queue/" + apiKey
-        l4g.Debug("Posting %s to queue %s", message.Body, queue)
-        c.Send(queue, message.Body)
-        c.Disconnect()
-        
-        //Return result to client
-        http.Error(w, "ok", 200)
-    }    
+		//Handle Auth
+		if solrServers[apiKey]["authstring"] != message.Authkey {
+			l4g.Error("Incorrect authkey")
+			http.Error(w, "Unauthorized", 401)
+			return
+		}
+
+		//Post message to queue
+		nc, err := net.Dial("tcp", ""+config["stomp"]["host"])
+		if err != nil {
+			l4g.Error("Error conneceting to queue %s", err.String())
+			http.Error(w, "Internal Server Error", 500)
+			return
+		}
+
+		//Note, the stomp module doesn't return os.Error on Send()
+		c, _ := stomp.Connect(nc, nil)
+		queue := "/queue/" + apiKey
+		l4g.Debug("Posting %s to queue %s", message.Body, queue)
+		nh := stomp.Header{}
+		c.Send(queue, nh,  message.Body)
+		c.Disconnect(nh)
+
+		//Return result to client
+		http.Error(w, "ok", 200)
+	}
 }
 
 func main() {
-    //Load logging configuration
-    l4g.LoadConfiguration("logging.xml")
-    
-    //Load default config file
-    var configPath string
-    flag.StringVar(&configPath, "config", "/etc/gosolr/gosolr.cfg", "Path to the configuration file")
-    flag.Parse()
-    config = loadConfig(configPath)
-    
-    //Load solr servers/cores from mysql db
-    solrServers = loadSolrServers(config)
-    prettyPrint(solrServers)
-    
-    //Setup the http proxy stuff
-    for apiKey := range solrServers {
-        urlPath := "/" + apiKey
-        http.HandleFunc(urlPath, handleRequest)
-    }
-    
-    var err os.Error
-    var srv http.Server
-    srv.Addr = config["default"]["host"] +":"+ config["default"]["port"]
-    srv.Handler = nil
-    if srv.ReadTimeout, err = strconv.Atoi64(config["default"]["read_timeout"]); err != nil {
-        l4g.Error("Configuration error. Bad read_timout value")
-        os.Exit(1)
-    }
-    if srv.WriteTimeout, err = strconv.Atoi64(config["default"]["write_timeout"]); err != nil {
-        l4g.Error("Configuration error. Bad write_timeout value")
-        os.Exit(1)
-    }
-    
-    //If this were real, this should be TLS
-    if err := srv.ListenAndServe(); err != nil {
-        l4g.Error("Error starting server: %s", err.String())
-        os.Exit(1)
-    }
+	//Load logging configuration
+	l4g.LoadConfiguration("logging.xml")
+
+	//Load default config file
+	var configPath string
+	flag.StringVar(&configPath, "config", "/etc/gosolr/gosolr.cfg", "Path to the configuration file")
+	flag.Parse()
+	config = loadConfig(configPath)
+
+	//Load solr servers/cores from mysql db
+	solrServers = loadSolrServers(config)
+	prettyPrint(solrServers)
+
+	//Setup the http proxy stuff
+	for apiKey := range solrServers {
+		urlPath := "/" + apiKey
+		http.HandleFunc(urlPath, handleRequest)
+	}
+
+	var err os.Error
+	var srv http.Server
+	srv.Addr = config["default"]["host"] + ":" + config["default"]["port"]
+	srv.Handler = nil
+	if srv.ReadTimeout, err = strconv.Atoi64(config["default"]["read_timeout"]); err != nil {
+		l4g.Error("Configuration error. Bad read_timout value")
+		os.Exit(1)
+	}
+	if srv.WriteTimeout, err = strconv.Atoi64(config["default"]["write_timeout"]); err != nil {
+		l4g.Error("Configuration error. Bad write_timeout value")
+		os.Exit(1)
+	}
+
+	//If this were real, this should be TLS
+	if err := srv.ListenAndServe(); err != nil {
+		l4g.Error("Error starting server: %s", err.String())
+		os.Exit(1)
+	}
 }
 
 func prettyPrint(printMap map[string]map[string]string) {
-    for key, tmpMap := range printMap {
-        for key2, value := range tmpMap {
-            l4g.Debug("%s ->\t%s -> %s", key, key2, value)
-        }
-    }
+	for key, tmpMap := range printMap {
+		for key2, value := range tmpMap {
+			l4g.Debug("%s ->\t%s -> %s", key, key2, value)
+		}
+	}
 }
--- gsconfy.go
+++ gsconfy.go
@@ -1,106 +1,106 @@
 package main
 
-import(
-    "mysql"
-    "conf"
-    "os"
-    l4g "log4go.googlecode.com/hg"
+import (
+	mysql "github.com/Philio/GoMySQL"
+	conf "goconf.googlecode.com/hg"
+	"os"
+	l4g "log4go.googlecode.com/hg"
 )
 
 type SolrData struct {
-    apistring   string
-    core        string
-    server      string
-    authstring  string
+	apistring  string
+	core       string
+	server     string
+	authstring string
 }
 
 //Move these into a different package?
-func loadConfig(file string) (map[string]map[string]string) {
-    config, err := conf.ReadConfigFile(file)
-    if err != nil {
-        l4g.Error("error reading config: %s\n", err.String())
-        os.Exit(1)
-    }
-    
-    //Setup configuration values
-    values := make(map[string]map[string]string)
-    
-    //Setup some default values - these are in the default namespace
-    //There are 3 configuration namespaces default, database, and stomp
-    //If the default value is "" that means it is required in the config file.
-    //An empty value in the config will not overwrite the default value
-    values["default"] = map[string]string{"host":"localhost", "port":"80", "read_timeout":"0", "write_timeout":"0"}
-    values["database"] = map[string]string{"host":"localhost", "port":"3306", "user":"", "pass":"", "name":""}
-    values["stomp"] = map[string]string{"host":""}
+func loadConfig(file string) map[string]map[string]string {
+	config, err := conf.ReadConfigFile(file)
+	if err != nil {
+		l4g.Error("error reading config: %s\n", err.String())
+		os.Exit(1)
+	}
 
-    l4g.Debug("Default vals: %v", values)
-    
-    //Read values from config
-    for namespace := range values {
-        //If there is a default value it's ok if the config key doesn't exist
-        for key, value := range values[namespace] {
-            if value != "" {
-                set := getValue(config, key, namespace, false)
-                if set != "" {
-                    values[namespace][key] = set
-                }
-            } else {
-                values[namespace][key] = getValue(config, key, namespace, true)
-            }
-        }
-    }
+	//Setup configuration values
+	values := make(map[string]map[string]string)
 
-    l4g.Debug("Final values: %v", values)
-    return values
+	//Setup some default values - these are in the default namespace
+	//There are 3 configuration namespaces default, database, and stomp
+	//If the default value is "" that means it is required in the config file.
+	//An empty value in the config will not overwrite the default value
+	values["default"] = map[string]string{"host": "localhost", "port": "80", "read_timeout": "0", "write_timeout": "0"}
+	values["database"] = map[string]string{"host": "localhost", "port": "3306", "user": "", "pass": "", "name": ""}
+	values["stomp"] = map[string]string{"host": ""}
+
+	l4g.Debug("Default vals: %v", values)
+
+	//Read values from config
+	for namespace := range values {
+		//If there is a default value it's ok if the config key doesn't exist
+		for key, value := range values[namespace] {
+			if value != "" {
+				set := getValue(config, key, namespace, false)
+				if set != "" {
+					values[namespace][key] = set
+				}
+			} else {
+				values[namespace][key] = getValue(config, key, namespace, true)
+			}
+		}
+	}
+
+	l4g.Debug("Final values: %v", values)
+	return values
 }
 
 func getValue(config *conf.ConfigFile, key string, namespace string, fail bool) string {
-    str, err := config.GetString(namespace, key)
-    if err != nil && fail {
-        //Exit if we can't find an expected value
-        l4g.Error("Error getting %s: %s\n", key, err.String())
-        os.Exit(1)
-    }
-    return str
+	str, err := config.GetString(namespace, key)
+	if err != nil && fail {
+		//Exit if we can't find an expected value
+		l4g.Error("Error getting %s: %s\n", key, err.String())
+		os.Exit(1)
+	}
+	return str
 }
 
 //Pull information from MySQL to find out which API keys we should handle and how they map
 //  i.e. servers and cores
-func loadSolrServers(config map[string]map[string]string) (map[string]map[string]string){
-    db, err := mysql.DialTCP(config["database"]["host"], config["database"]["user"], config["database"]["pass"], config["database"]["name"])
-    if err != nil {
-        l4g.Error("Error connecting to db: %s\n", err.String())
-        os.Exit(1)
-    }
-    stmt, err := db.Prepare("Select apistring,core,server,authstring from cores where gosolr = ?")
-    if err != nil {
-        l4g.Error("Error preparing statement: %s", err.String())
-        os.Exit(1)
-    }
-    
-    stmt.BindParams(config["default"]["host"])
-    
-    err = stmt.Execute()
-    if err != nil { 
-        l4g.Error("error executing stmt: %s", err.String())
-        os.Exit(1)
-    }    
-    
-    var solrrow SolrData
-    stmt.BindResult(&solrrow.apistring, &solrrow.core, &solrrow.server, &solrrow.authstring)
-    solr_values := make(map[string]map[string]string)
-    
-    for {
-        eof, err := stmt.Fetch()
-        if err != nil {
-            l4g.Error("Error fetching row: %s", err.String())
-            os.Exit(1)
-        }
-        
-        solr_values[solrrow.apistring] = map[string]string{"core":solrrow.core, "server":solrrow.server, "authstring":solrrow.authstring}
-        if eof {
-            break
-        }
-    }
-    return solr_values
+func loadSolrServers(config map[string]map[string]string) map[string]map[string]string {
+	db, err := mysql.DialTCP(config["database"]["host"], config["database"]["user"], config["database"]["pass"], config["database"]["name"])
+	if err != nil {
+		l4g.Error("Error connecting to db: %s\n", err.String())
+		os.Exit(1)
+	}
+	stmt, err := db.Prepare("Select apistring,core,server,authstring from cores where gosolr = ?")
+	if err != nil {
+		l4g.Error("Error preparing statement: %s", err.String())
+		os.Exit(1)
+	}
+
+	stmt.BindParams(config["default"]["host"])
+
+	err = stmt.Execute()
+	if err != nil {
+		l4g.Error("error executing stmt: %s", err.String())
+		os.Exit(1)
+	}
+
+	var solrrow SolrData
+	stmt.BindResult(&solrrow.apistring, &solrrow.core, &solrrow.server, &solrrow.authstring)
+	solr_values := make(map[string]map[string]string)
+
+	for {
+		eof, err := stmt.Fetch()
+		if err != nil {
+			l4g.Error("Error fetching row: %s", err.String())
+			os.Exit(1)
+		}
+
+		solr_values[solrrow.apistring] = map[string]string{"core": solrrow.core, "server": solrrow.server, "authstring": solrrow.authstring}
+		if eof {
+			break
+		}
+	}
+	return solr_values
 }
